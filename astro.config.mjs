a06760a97a5ecc6a323b050876975430414b79d0
ckt <cacao.chen@heavengifts.com>
2026-02-09 10:55:03 +0800

    feat: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import path from 'path';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import path from "path";
 
 export default defineConfig({
   // 替换成你自己的 GitHub 地址
-  site: 'https://mh0904.github.io', 
-  
-  // 显式指定为静态模式，防止它乱跑
-  output: 'static', 
+  site: "https://mh0904.github.io",
+  base: "/mh-blog",
 
-  integrations: [
-    tailwind(), 
-    sitemap()
-  ],
+  // 显式指定为静态模式，防止它乱跑
+  output: "static",
+
+  integrations: [tailwind(), sitemap()],
 
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('src')
-      }
-    }
-  }
+        "@": path.resolve("src"),
+      },
+    },
+  },
   // 先不要加 markdown 插件和 image service，等页面跑通了再加
 });
